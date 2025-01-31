6e2576c7223d44892f37e0b28bd7b477f45affc2
lucbrd8 <lucbiard57@gmail.com>
2025-01-31 11:54:51 +0100

    code luc

--- Luc.cpp
+++ Luc.cpp
@@ -1,3 +1,20 @@
-//
-// Created by lasma on 31/01/2025.
-//
+#include <iostream>
+#include <cstdlib>
+#include "simon.h"
+#include "Simon.cpp"
+bool salle_possible (Salle s, char donjon) {
+    for (int i=0, i<s.longueur, i++) {
+        for (int j=0, j<s.largeur,j++) {
+            int x =s.origin[0]+i;
+            int y = s.origin[1] +j
+            if (x<LENGHT_BOARD and y<WIDTH_BOARD) {
+                if(donjon[x][y]!=' ') {
+                    return false
+                }
+            } else {
+                return false
+            }
+        }
+    }
+    return true;
+    }
