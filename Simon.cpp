21346c76c3e1faab386ec1677d6606a504c7dc9a
Simon Lamaze <simon.lamaze@etu.minesparis.psl.eu>
2025-01-31 11:30:23 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/editor.xml

--- Simon.cpp
+++ Simon.cpp
@@ -1,3 +1,61 @@
+#include "simon.h"
+#include <iostream>
+#include<array>
+using namespace std;
+// Created by lasma o
+// n 31/01/2025.
 //
-// Created by lasma on 31/01/2025.
-//
+
+char donjon[50][50] ;
+void initialize() {
+    for(int i=0;i<50;i++) {
+        for(int j=0;j<50;j++) {
+            donjon[i][j]=' ';
+        }
+    }
+}
+
+
+void addroom(Salle s) {
+    array<int , 2> start = s.origin ;
+    int a = s.longueur;
+    int b = s.largeur ;
+    for  (int i =0; i< a+1; i++) {
+        donjon[start[0]+i][start[1]] = '-' ;
+    }
+    for  (int i =1; i< b; i++) {
+        donjon[start[0]][start[1]+i] = '|' ;
+        for (int j =1; j< a; j++) {
+
+            donjon[start[0]+j][start[1]+i] = '.';
+        }
+        donjon[start[0]+a][start[1]+i] = '|' ;
+    }
+    for  (int i =0; i< a+1; i++) {
+        donjon[start[0]+i][start[1]+b] = '-' ;
+    }
+    for (auto entry : s.entrances) {
+        donjon[entry[0]+start[0]][entry[1]+start[1]] = '+' ;
+    }
+
+}
+void printDonjon() {
+    for (int i =0; i< 50; i++) {
+        for (int j =0; j< 50; j++) {
+            cout << donjon[j][i];
+        }
+        cout << endl;
+    }
+}
+
+
+int main(){
+    initialize();
+    Salle Room1 = Salle(5,5,{10,10},{{0,4},{5,2}});
+    addroom(Room1);
+    printDonjon();
+
+
+
+    return 0;
+}
--- simon.h
+++ simon.h
@@ -1,8 +1,26 @@
 //
 // Created by lasma on 31/01/2025.
 //
+#include<iostream>
+#include<array>
+#include<vector>
+
+using namespace std;
 
 #ifndef SIMON_H
 #define SIMON_H
 
 #endif //SIMON_H
+
+
+
+ class Salle {
+ public:
+     int longueur;
+     int largeur; // nombre de cases vides +2 ( prend en compte les murs)
+     array<int,2> origin ; // en bas à gauche du rectangle en 0 la coordonnée horizontale ( longueur) , en 1 la verticale (largeur)
+     vector<array<int,2> > entrances; // liste des coordonnées des entrées, avec le 0 à origine
+     Salle(int longueur , int largeur,array<int, 2> origin,vector<array<int,2> > entrances): longueur(longueur),largeur(largeur),origin(origin),  entrances(entrances) {}
+
+
+ };
